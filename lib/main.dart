9db85b69b09633ac5cddbc154d2bd40adbbe91f5
Sadia <you@example.com>
2025-05-12 10:31:18 +0600

    initialized firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; 
 import 'routes/app_routes.dart';
 import 'core/theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //  Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -15,8 +24,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Notes App',
-     
       routerConfig: AppRoutes.router,
+      theme: AppTheme.lightTheme, // Optional if you have a theme
     );
   }
 }
